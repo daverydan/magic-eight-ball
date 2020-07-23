cc5631ca7e47313e9cf683a5096571febdd60382
Danny Avery <danny.w.avery@gmail.com>
2020-07-23 13:13:01 -0500

    Updates

---
+++ precache-manifest.105a032bbcabf4e87dcf893f27dd755d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374e56ff0a11ad8fda5bcaad04ffed64",
+    "url": "/magic-eight-ball/index.html"
+  },
+  {
+    "revision": "a05c00c006a60d3b4e29",
+    "url": "/magic-eight-ball/static/css/main.db1f9613.chunk.css"
+  },
+  {
+    "revision": "6a94b56bddecd484f014",
+    "url": "/magic-eight-ball/static/js/2.f236db9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magic-eight-ball/static/js/2.f236db9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a05c00c006a60d3b4e29",
+    "url": "/magic-eight-ball/static/js/main.8a161f34.chunk.js"
+  },
+  {
+    "revision": "ae068f0e22904820114e",
+    "url": "/magic-eight-ball/static/js/runtime-main.34e40b17.js"
+  }
+]);
